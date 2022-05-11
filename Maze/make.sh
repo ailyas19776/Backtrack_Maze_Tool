50ab5f1fa53ef464bfc4dfb44d12f026d7601bfa
ailyas19776 <97473935+ailyas19776@users.noreply.github.com>
2022-05-10 22:11:08 -0400

    Update make.sh

--- Maze/make.sh
+++ Maze/make.sh
@@ -1,3 +1,3 @@
 g++ -std=c++1z -Wconversion -Wall -Werror -Wextra -pedantic  -O3 -DNDEBUG -c stackQueue.cpp
 g++ -std=c++1z -Wconversion -Wall -Werror -Wextra -pedantic  -O3 -DNDEBUG -c main.cpp
-g++ -std=c++1z -Wconversion -Wall -Werror -Wextra -pedantic  -O3 -DNDEBUG stackQueue.o main.o -o superMarco
+g++ -std=c++1z -Wconversion -Wall -Werror -Wextra -pedantic  -O3 -DNDEBUG stackQueue.o main.o -o main
